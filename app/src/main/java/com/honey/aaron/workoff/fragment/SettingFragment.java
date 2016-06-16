5ae213e3d3d5f7c236b850968efb7e8fd3381df6
Aaron Choi <wolfjunghun@naver.com>
2016-06-16 21:45:18 +0900

    test commit
    Change-Id: Ie2a1db57e95ecafbfd2ed297ae5580a7e55645df

--- app/src/main/java/com/honey/aaron/workoff/fragment/SettingFragment.java
+++ app/src/main/java/com/honey/aaron/workoff/fragment/SettingFragment.java
@@ -2,6 +2,9 @@ package com.honey.aaron.workoff.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingFragment extends BaseFragment {
     static SettingFragment settingFragment;
@@ -22,5 +25,10 @@ public class SettingFragment extends BaseFragment {
         super.onCreate(savedInstanceState);
 
         TAG = SettingFragment.class.getSimpleName();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
